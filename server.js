8cf7d7fc3f4c85699e9a04376c696899f02ea03b
jjmchew <jjmchew@gmail.com>
2024-05-06 06:59:53 -0600

    express server added

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import db from './db.json' assert { type: 'json' };
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.get('/workouts', (_req, res) => {
+  res.send(db);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
