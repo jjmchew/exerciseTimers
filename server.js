4795e70a9027e62f5ce8d42043ab06cb8a24a66a
James Chew <jjmchew@gmail.com>
2025-10-30 12:25:12 -0600

    chore: package and script clean-up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import db from './db.json' assert { type: 'json' };
+import express from "express";
+import cors from "cors";
+import db from "./db.json" with { type: "json" };
 
 const app = express();
 
@@ -9,15 +9,14 @@ app.use(express.json());
 
 const PORT = 3003;
 
-app.get('/ping', (_req, res) => {
-  res.send('pong');
+app.get("/ping", (_req, res) => {
+  res.send("pong");
 });
 
-app.get('/workouts', (_req, res) => {
+app.get("/workouts", (_req, res) => {
   res.send(db.workouts);
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
