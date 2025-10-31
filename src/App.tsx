87c176a9c73054111c97aa024f809f7e250808e9
James Chew <jjmchew@gmail.com>
2025-10-30 22:41:02 -0600

    fix: app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
-import { useState, useEffect } from 'react'
-import { Workout, Activity } from './types.ts';
-import API from './components/api.ts';
-import WorkoutSelect from './components/WorkoutSelect.tsx';
-import ActivityDisplay from './components/ActivityDisplay.tsx';
-import TimerWrapper from './components/TimerWrapper.tsx';
-import utils from './utils.ts';
-import './App.css'
+import { useState, useEffect } from "react";
+import { Workout, Activity } from "./types.ts";
+import API from "./components/api.ts";
+import WorkoutSelect from "./components/WorkoutSelect.tsx";
+import TimersList from "./components/TimersList.tsx";
+import utils from "./utils.ts";
+import "./App.css";
 
 function App() {
   const [workouts, setWorkouts] = useState<Workout[] | null>(null);
-  const [selected, setSelected] = useState<string>('');
+  const [selected, setSelected] = useState<string>("");
   const activities: Activity[] | null = utils.getActivities(selected, workouts);
 
   useEffect(() => {
     if (workouts === null) {
-      API.getData()
-        .then(data => setWorkouts(data));
+      API.getData().then((data) => setWorkouts(data));
     }
   }, []);
-
 
   const handleSelect = (e: string) => {
     setSelected(e);
   };
 
-  if (!workouts) return (<p>loading...</p>);
+  if (!workouts) return <p>loading...</p>;
 
   return (
     <>
-      <h1>Exercise timer</h1>
+      <h1>Exercise timers</h1>
       <WorkoutSelect workouts={workouts} handleSelect={handleSelect} />
-      <ActivityDisplay activities={activities} />
-      <TimerWrapper activities={activities} />
+      <TimersList key={selected} activities={activities} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
