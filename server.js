620ce20ca49658e36e9858335d7e8514d62db4af
James Chew <jjmchew@gmail.com>
2025-11-01 12:18:58 -0600

    feat: read db.json on request

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import db from "./db.json" with { type: "json" };
+import { readFile } from "fs/promises";
 
 const app = express();
 
@@ -9,12 +9,25 @@ app.use(express.json());
 
 const PORT = 3003;
 
+async function loadJson() {
+  try {
+    const filePath = new URL("./db.json", import.meta.url);
+    const contents = await readFile(filePath, { encoding: "utf8" });
+    return JSON.parse(contents);
+  } catch (error) {
+    console.error("Error loading db.json", error);
+    return null;
+  }
+}
+
 app.get("/ping", (_req, res) => {
   res.send("pong");
 });
 
-app.get("/workouts", (_req, res) => {
-  res.send(db.workouts);
+app.get("/workouts", async (_req, res) => {
+  const data = await loadJson();
+  if (data) res.send(data.workouts);
+  else res.send({});
 });
 
 app.listen(PORT, () => {
